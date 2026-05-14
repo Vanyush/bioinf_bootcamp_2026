90c94a57e824c16d757feaa291abb0f98dc192fc
Евгений Федоров <darkyducky476@gmail.com>
2026-05-14 22:56:39 +0300

    Create bioinformatics_bootcampApp.swift

---
+++ mmnv_swift/bioinformatics_bootcamp_source_code/bioinformatics_bootcampApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct ModernMitochondrialNetworkVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.automatic)
+    }
+}
